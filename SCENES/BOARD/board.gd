71460a064ad5a0be18e6d57fea07711582863c9e
emilkattel <emil1kattel@gmail.com>
2026-04-21 22:18:00 +0300

    dicedicedice

--- SCENES/BOARD/board.gd
+++ SCENES/BOARD/board.gd
@@ -2,23 +2,59 @@ extends Node3D
 
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var dice_container: Node3D = $dice_container
+@onready var button: Button = $UI/Button
+
+
+@export var rerolls_remaining:int  = 2
 
 var dice_scene = preload("res://SCENES/DICE/dice.tscn")
-
+var all_dice = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in 5:
-		spawn_dice()
-	pass # Replace with function body.
+	spawn_dice()
+	update_button_text()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
+
 func spawn_dice():
 	var dice = dice_scene.instantiate()
 	dice_container.add_child(dice)
-	dice.position = Vector3(randf_range(0, 0.5), 0.2, randf_range(0, 0.5))
-	dice.apply_central_impulse(Vector3(randf_range(-0.5, 0.5), 0, randf_range(-0.5, 0.5)))
-	dice.apply_torque_impulse(Vector3(randf_range(-5, 5), randf_range(-5, 5), randf_range(-5, 5)))
+	
+	var sides = [
+		{"value": 2, "effects": ["attack"]},
+		{"value": 1, "effects": ["shield"]},
+		{"value": 3, "effects": ["poison"]},
+		{"value": 1, "effects": ["attack", "poison"]},
+		{"value": 0, "effects": []},
+		{"value": 4, "effects": ["attack"]}
+	]
+	dice.set_sides(sides)
+	dice.reset_and_reroll()
+	all_dice.append(dice)
+
+
+func update_button_text():
+	if rerolls_remaining == 1:
+		button.text = "1 REROLL"
+	elif rerolls_remaining == 0:
+		button.text = "0 REROLLS"
+	else:
+		button.text = "%d REROLLS" % [rerolls_remaining]
+	
+func reroll_dice():
+	if rerolls_remaining <= 0:
+		print("no rerolls")
+		return
+	rerolls_remaining -= 1
+	for dice in all_dice:
+		if not dice.is_locked:
+			dice.reset_and_reroll()
+	update_button_text()
+
+func _on_button_pressed() -> void:
+	reroll_dice()
--- SCENES/DICE/dice.gd
+++ SCENES/DICE/dice.gd
@@ -1,11 +1,91 @@
 extends RigidBody3D
 
 var sides_data = []
+var is_settled: bool = false
+var settle_threshold: float = 0.1
+var is_locked: bool = false
+@onready var faces = [
+	$face0,
+	$face1,
+	$face2,
+	$face3,
+	$face4,
+	$face5
+	]
+@onready var mesh_instance = MeshInstance3D
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	await get_tree().process_frame
+	update_mesh_colors()
+	
+
+func set_sides(data: Array) -> void:
+	sides_data = data
+	if is_node_ready():
+		update_mesh_colors()
+		
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_effect_color(effects: Array) -> Color:
+	# Priority: if multiple effects, pick the first one
+	if effects.is_empty():
+		return Color.GRAY  # Blank side
+	
+	match effects[0]:
+		"attack":
+			return Color.RED
+		"shield":
+			return Color.BLUE
+		"poison":
+			return Color.GREEN
+		_:
+			return Color.WHITE
+
+func update_mesh_colors() -> void:
+	if not is_node_ready():
+		return
+	for i in range(faces.size()):
+		var face_mesh = faces[i]
+		var material = StandardMaterial3D.new()
+		material.albedo_color = get_effect_color(sides_data[i]["effects"])
+		face_mesh.set_surface_override_material(0, material)
+		
+func _physics_process(delta: float) -> void:
+	if is_locked:
+		return
+	if not is_settled:
+		await get_tree().physics_frame
+		if linear_velocity.length() < settle_threshold and angular_velocity.length() < settle_threshold:
+			is_settled = true
+			var top_face = get_top_face()
+			var face_data = sides_data[top_face]
+			print("Top face: ", top_face, " | Value: ", face_data["value"], " | Effects: ", face_data["effects"])
+			
+func get_top_face() -> int:
+	
+	var max_up = -1.0
+	var top_face_index = 0
+	
+	# Check which face's normal points most upward
+	for i in range(faces.size()):
+		var face = faces[i]
+		# Get the face's Y position to determine if it's pointing up
+		var face_y = face.global_position.y
+		
+		# The face that's highest is the top
+		if face_y > max_up:
+			max_up = face_y
+			top_face_index = i
+	
+	return top_face_index
+
+func toggle_lock() -> void:
+	is_locked = !is_locked
+	print("dice locked: ", is_locked)
+	
+
+func reset_and_reroll() -> void:
+	is_settled = false
+	position = Vector3(randf_range(0, 0.1), -1, randf_range(0, 0.1))
+	apply_central_impulse(Vector3(randf_range(-10, 10), 0, randf_range(-10, 10)))
+	apply_torque_impulse(Vector3(randf_range(-0.05, 0.05), randf_range(-0.05, 0.05), randf_range(-0.05, 0.05)))
